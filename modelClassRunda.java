1c0ed73d6a5f7e11fb677fe389f4ac4a705bd9c8
NGERESA ENOSH <134959771+NgeresaEnosh@users.noreply.github.com>
2024-04-25 21:26:26 +0300

    modelClassRunda.java

---
+++ modelClassRunda.java
@@ -0,0 +1,30 @@
+package com.example.rental;
+
+public class ModelClassRunda {
+        String apartmentName, apartmentNum;
+        int img;
+
+        public String getApartmentName() {
+            return apartmentName;
+        }
+
+        public void setApartmentName(String apartmentName) {
+            this.apartmentName = apartmentName;
+        }
+
+        public String getApartmentNum() {
+            return apartmentNum;
+        }
+
+        public void setApartmentNum(String apartmentNum) {
+            this.apartmentNum = apartmentNum;
+        }
+
+        public int getImg() {
+            return img;
+        }
+
+        public void setImg(int img) {
+            this.img = img;
+        }
+    }
