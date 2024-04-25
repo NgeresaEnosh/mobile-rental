22015a3b6564fcec44471dc434e54b09a6013be7
NGERESA ENOSH <134959771+NgeresaEnosh@users.noreply.github.com>
2024-04-25 21:25:12 +0300

    modelClassMolongo.java

---
+++ modelClassMolongo.java
@@ -0,0 +1,31 @@
+package com.example.rental;
+
+public class ModelClassMlolongo {
+
+    String apartmentName, apartmentNum;
+    int img;
+
+    public String getApartmentName() {
+        return apartmentName;
+    }
+
+    public void setApartmentName(String apartmentName) {
+        this.apartmentName = apartmentName;
+    }
+
+    public String getApartmentNum() {
+        return apartmentNum;
+    }
+
+    public void setApartmentNum(String apartmentNum) {
+        this.apartmentNum = apartmentNum;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+}
